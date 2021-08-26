7585d409555e4efb55603581dee42387da272ff9
ABIR PAUL <65394557+wandererabir@users.noreply.github.com>
2021-08-26 21:21:15 +0530

    :100:

---
+++ js/kodoshinobi.js
@@ -0,0 +1,4 @@
+// Declaring global array which stores url of image of landing page
+let landingPageImg=['landingPage1.jpg','landingPage2.jpg','landingPage3.jpg']
+// Declaring variable which will keep track of the no. of times the up and down button clicked and increment and decrement accordingly
+let btnClickedCount=0
